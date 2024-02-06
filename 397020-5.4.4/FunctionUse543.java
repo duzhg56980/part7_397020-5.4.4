b6ff7871012cf919c8e1823e629620c8c5df5476
duzhg56980 <guokey1996@gmail.com>
2024-02-06 22:09:44 +0800

    init commit

---
+++ 397020-5.4.4/FunctionUse543.java
@@ -0,0 +1,6 @@
+import cn.hutool.http.webservice.SoapRuntimeException;
+public class FunctionUse543 {
+public void funcUse() {
+SoapRuntimeException soapruntimeexception = new SoapRuntimeException();
+}
+}
